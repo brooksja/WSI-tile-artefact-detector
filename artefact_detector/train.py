cf308fcb78246146f7a0031560888e9c6f109ede
James Brooks <james.alex.brooks@gmail.com>
2023-07-11 10:50:09 +0200

    completed switch to custom CNN for anomaly detection

--- artefact_detector/train.py
+++ artefact_detector/train.py
@@ -1,28 +1,34 @@
 # Attempt at creating an anomaly detector to flag WSI tiles with artefacts/weird colours/etc
 
-# Following https://pyimagesearch.com/2020/01/20/intro-to-anomaly-detection-with-opencv-computer-vision-and-scikit-learn/
+from model import Artefact_detector
+import torch
+from torchvision.datasets import ImageFolder
+from torchvision import transforms as T
+from torch.utils.data import random_split,DataLoader
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+import os
 
-from helpers import load_dataset
-from sklearn.ensemble import IsolationForest
-import argparse
-import pickle
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-	help="path to dataset of images")
-ap.add_argument("-m", "--model", required=True,
-	help="path to output anomaly detection model")
-args = vars(ap.parse_args())
+torch.set_float32_matmul_precision('medium')
 
-# load and quantify our image dataset
-print("[INFO] preparing dataset...")
-data = load_dataset(args["dataset"], bins=(4,6,3))
-# train the anomaly detection model
-print("[INFO] fitting anomaly detection model...")
-model = IsolationForest(n_estimators=100,random_state=42)
-model.fit(data)
+transform = T.Compose([
+    T.ToTensor(),
+    T.Resize(225,antialias=True),
+    T.CenterCrop(224),
+    T.RandomHorizontalFlip(),
+    T.RandomVerticalFlip(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+])
 
-# serialize the anomaly detection model to disk
-f = open(args["model"], "wb")
-f.write(pickle.dumps(model))
-f.close()
+dataset = ImageFolder(
+    root = '/mnt/ravenclaw/Artefact_dataset/',
+    transform=transform
+)
+
+train,valid = random_split(dataset,[0.8,0.2])
+train_loader = DataLoader(train,batch_size=64,num_workers=os.cpu_count(),shuffle=True)
+valid_loader = DataLoader(valid,batch_size=64,num_workers=os.cpu_count())
+
+model = Artefact_detector()
+trainer = pl.Trainer(accelerator='gpu',max_epochs=100,callbacks=[EarlyStopping(monitor='valid_loss',min_delta=1e-4,patience=5)],log_every_n_steps=10)
+trainer.fit(model,train_loader,valid_loader)
